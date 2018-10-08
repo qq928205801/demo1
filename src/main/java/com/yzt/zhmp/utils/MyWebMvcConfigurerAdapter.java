4feed94c8a34b25d50d4aa3c26a97df3107c7e87
wang <wangyinglong3@foxmail.com>
2018-10-08 15:10:11 +0800

    2018-10-08 15:02 添加登录拦截器 将静态资源复制一份到resource文件夹下方便访问 application.properties文件进行了修改添加useSSL=false和静态文件访问配置 修改control/index标签栏的id避免重复

---
+++ src/main/java/com/yzt/zhmp/utils/MyWebMvcConfigurerAdapter.java
@@ -0,0 +1,33 @@
+package com.yzt.zhmp.utils;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+/**
+ * @author wang
+ * 自定义配置拦截器配置
+ */
+@Configuration
+public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
+
+    /**
+     * 配置静态资源
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backstageResource/**").addResourceLocations("classpath:/backstageResource/");
+       registry.addResourceHandler("/control/**").addResourceLocations("classpath:/control/");
+       registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration interceptorRegistration = registry.addInterceptor(new LoginInterceptor());
+        interceptorRegistration.addPathPatterns("/**");
+        interceptorRegistration.excludePathPatterns("/control/index");
+        interceptorRegistration.excludePathPatterns("/backstageResource/**");
+
+
+    }
+}
